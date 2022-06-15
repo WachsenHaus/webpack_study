fac475b63f95fa0a410533676ea6154041cf82d0
Younghoon CHOI _^ <bi9choi@gmail.com>
2022-06-15 14:13:08 +0900

    add : 모듈 방식의 종류들 commonjs amd umd es2015에서 표준 모듈 시스템을 내놓음.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log(sum(1, 2));
+import * as moduleMath from './math.js';
+console.log('모듈방식');
+console.log(moduleMath.sum(1, 2));
+console.log('iife방식');
+// console.log(math.sum(1, 2));
--- src/math.js
+++ src/math.js
@@ -1,3 +1,13 @@
-function sum(a, b) {
+// var math = math || {};
+
+// (function () {
+//   function sum(a, b) {
+//     return a + b;
+//   }
+//   math.sum = sum;
+// })();
+
+// 표쥰 모듈방식
+export function sum(a, b) {
   return a + b;
 }
